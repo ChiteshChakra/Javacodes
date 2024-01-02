57e85311e678fa527445ca5b70d4cb4400a60866
ChiteshChakra <chiteshchinna2000@gmail.com>
2024-01-02 15:43:37 +0530

    practise.java

--- Practise.java
+++ Practise.java
@@ -1,20 +1,33 @@
 import java.util.*;
 
-import javax.annotation.processing.SupportedOptions;
 public class Practise {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        // String input = sc.next();
-        // System.out.println(input);
         
-        //String name = sc.nextLine();
-        //System.out.println(name);
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        System.out.println(input);
+        
+        String name = sc.nextLine();
+        System.out.println(name);
 
         int number = sc.nextInt();
         System.out.println(number);
 
-         float price =sc.nextFloat();
-         System.out.println(price);
+        float price =sc.nextFloat();
+        System.out.println(price);
+        inputTest();
     }
+
+    public static void inputTest (){
+        Scanner sc=new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a + b;
+        int product = a * b;
+        System.out.println(sum);
+        System.out.println(product);
+    } 
+    
+
     
 }
