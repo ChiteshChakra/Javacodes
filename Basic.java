812c0e05ea74c562f096b185b14f680a5841ff71
ChiteshChakra <chiteshchinna2000@gmail.com>
2024-01-03 23:18:17 +0530

    Basic2

--- Basic.java
+++ Basic.java
@@ -106,7 +106,29 @@ public class Basic{
 
         String reportCard = marks >= 33 ? "PASS" : "FAIL";
         System.out.println(reportCard);
+    
+       sstatement();
     }
+
+    public static void sstatement(){
+        char ch='a';
+        switch(ch){
+            case 'd' : System.out.println("samosa");
+                        break;
+            case 'a' : System.out.println("burger");
+                        break;
+            case 'c' : System.out.println("mango shake");
+                        break;
+            default : System.out.println("we wake up");
+
+
+        }
+        
+
+    }
+    
+    
+
 
     
 }
