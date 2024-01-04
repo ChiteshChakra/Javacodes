d67f7f7520a8e69818d5b610cc980d0abbacafb6
ChiteshChakra <chiteshchinna2000@gmail.com>
2024-01-04 12:29:24 +0530

    for loop in java

---
+++ Loop2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Loop2 {
+    
+    public static void main(String[] args) {
+        for(int  i=1;i<=10;i++){
+            System.out.print("Guru ");
+
+        }
+        System.out.println();
+
+        
+    }
+}
