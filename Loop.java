67076a31aa6b302522d2ed7f41f1d783aa1a7044
ChiteshChakra <chiteshchinna2000@gmail.com>
2024-01-04 17:21:31 +0530

    loops in java

--- Loop.java
+++ Loop.java
@@ -116,6 +116,22 @@ public class Loop{
     }
     System.out.println("out of the loop");
 
+    user();
+    }
+    
+
+    public static void user(){
+        Scanner sc = new Scanner(System.in);
+        
+        do {
+            System.out.println("enter your number");
+            int n = sc.nextInt();
+            if(n%10 == 0){
+                break;
+            }
+            System.out.println(n);
+            
+        }while(true);
 
     }
     
