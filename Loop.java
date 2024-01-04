c14cfde7d569bea2b6cb2ba25b65cf3d6683eb01
ChiteshChakra <chiteshchinna2000@gmail.com>
2024-01-04 10:44:27 +0530

    loops in java

---
+++ Loop.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Loop {
+    public static void main (String args[]){
+        int counter = 0;
+        while(counter < 15){
+            System.out.println("hello");
+            counter++; ///why 
+        }
+        System.out.println("printer hello 15x");
+
+    }
+}
