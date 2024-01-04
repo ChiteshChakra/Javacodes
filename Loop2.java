080f0732000f82cec6bfcb2230400fbe3b280a56
ChiteshChakra <chiteshchinna2000@gmail.com>
2024-01-04 13:10:00 +0530

    reverse of number by storing

--- Loop2.java
+++ Loop2.java
@@ -31,5 +31,21 @@ public class Loop2 {
             n=n/10;// to remove last digit  from the number
         }
             System.out.println();
+
+            reversenumber2();
         }
+    
+    public static void reversenumber2(){
+        Scanner sc = new  Scanner (System.in);
+        int n = sc. nextInt();
+        int rev=0;
+        int lastdigit;
+        while ( n>0){
+            lastdigit = n%10;
+            rev =(rev*10) +lastdigit;
+            n=n/10;
+        }
+        System.out.println();
+
+    }
 }
