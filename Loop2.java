8c5f218478c65a2b0fed01016b9526308a3cc863
ChiteshChakra <chiteshchinna2000@gmail.com>
2024-01-04 12:59:27 +0530

    reverse of number

--- Loop2.java
+++ Loop2.java
@@ -9,6 +9,27 @@ public class Loop2 {
         }
         System.out.println();
 
-        
+        pattern();
     }
+    public static void pattern(){
+        int lines = 1;
+        for (lines =1;lines<=4;lines++){
+            System.out.println("****");
+        }
+
+       reversenumber();
+        }
+    
+    public static void reversenumber(){
+        Scanner sc= new Scanner (System.in);
+        System.out.println("enter a number:");
+        int n =sc.nextInt();
+        int lastdigit;
+        while(n>0){
+            lastdigit=n%10;// mod of any number by 10 gives last digit of the number
+            System.out.print(lastdigit);
+            n=n/10;// to remove last digit  from the number
+        }
+            System.out.println();
+        }
 }
